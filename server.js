c15846094d94e80ce5ebddde2c4649bbc1e8817d
Maciej Lichoń <maciek.lichon@gmail.com>
2022-05-27 23:22:51 +0200

    add form

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer  = require('multer');
 
 const app = express();
+const upload = multer();
 
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' })); // dane pliki o rozszerzeniu .hbs powinny byc renderowane przez dany silnik hbs
 app.set('view engine', '.hbs'); // informacja o rozszerzeniu wykorzystywanych plikow
 
 
 app.use(express.static(path.join(__dirname, '/public')));
-
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -33,6 +35,21 @@ app.get('/history', (req, res) => {
 
 app.get('/hello/:name', (req, res) => {
   res.render('hello', { name: req.params.name });
+});
+
+
+app.post('/contact/send-message', upload.single('uploaded_image'), (req, res) => {
+
+  const { author, sender, title, message } = req.body;
+  const image = req.file;
+
+  if (author && sender && title && message && image) {
+    res.render('contact', { isSent: true, imageName: image.originalname });
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
+
 });
 
 
